1b064cdd6932fab6a11ba825f0ae8bac8ab519da
jks77 <jankrijn@gmail.com>
2020-03-23 17:36:40 +0100

    added rotation when starting game and refactored function checkWinner

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ let box9 = boxes[8];
 let currentPlayer;
 
 let playerOne = {
-	token: 'x',
+	token: 'X',
 	name: 'player one'
 };
 
 let playerTwo = {
-	token: 'o',
+	token: 'O',
 	name: 'player two'
 };
 
 let draw;
 let gameEnded = false;
 
-// -------------------- einde variabelen-------------------
+// einde variabelen, opmaak startscherm
 
 h2.style.display = 'none';
 
@@ -41,26 +41,28 @@ btn.addEventListener('click', function() {
 	startGame();
 });
 
+// functie om spel te (her)starten
+
 function startGame() {
 	gameEnded = false;
 	draw = 0;
 	currentPlayer = playerOne;
-	h2.innerHTML = currentPlayer.name;
+	h2.innerText = currentPlayer.name;
 
-	// voor het verdwijnen van spelbord naar achtergrond en weer verschijnen
-	container.classList.add('small');
+	// zorgt voor verdwijnen van spelbord naar achtergrond en weer verschijnen
+	container.classList.add('rotate');
 	setTimeout(function() {
-		container.classList.remove('small');
+		container.classList.remove('rotate');
 		h2.style.display = 'block';
 		btn.style.display = 'none';
 		for (let i = 0; i < boxes.length; i++) {
-			boxes[i].innerHTML = '';
+			boxes[i].innerText = '';
 			boxes[i].classList.add('hover');
 			boxes[i].classList.remove('start');
 			boxes[i].classList.remove('no-hover');
 			boxes[i].classList.remove('when-won');
 		}
-	}, 1400);
+	}, 1500);
 }
 
 // functie om op elke box een mouseenter event te zetten om de styling te beheersen
@@ -72,6 +74,15 @@ function makeMouseEnterEvent() {
 	}
 }
 
+function checkBoxToken() {
+	if (event.target.innerText === '') {
+		event.target.innerText = currentPlayer.token;
+		event.target.style.color = 'black';
+	}
+}
+
+makeClickEventToSetToken();
+
 // functie om op elke box een mouseleave event te zetten om de styling te beheersen
 function makeMouseLeaveEvent() {
 	for (let i = 0; i < boxes.length; i++) {
@@ -81,7 +92,15 @@ function makeMouseLeaveEvent() {
 	}
 }
 
-// functie om op elke box een click event te zetten om per speler het token te kunnen zetten
+function leaveBox() {
+	if (event.target.style.color === 'black') {
+		event.target.innerText = '';
+	}
+}
+
+makeMouseLeaveEvent();
+
+// functie om op elke box een click event te zetten om per speler zijn token te kunnen zetten
 function makeClickEventToSetToken() {
 	for (let i = 0; i < boxes.length; i++) {
 		boxes[i].addEventListener('click', function() {
@@ -90,45 +109,29 @@ function makeClickEventToSetToken() {
 	}
 }
 
-makeMouseEnterEvent();
-makeMouseLeaveEvent();
-makeClickEventToSetToken();
-
-function checkBoxToken() {
-	if (event.target.innerHTML === '') {
-		event.target.innerHTML = currentPlayer.token;
-		event.target.style.color = 'black';
-	}
-}
-
-function leaveBox() {
-	if (event.target.style.color === 'black') {
-		event.target.innerHTML = '';
-	}
-}
-
 function setToken() {
 	let element = event.target;
 	if (element.style.color === 'black') {
-		element.innerHTML = currentPlayer.token;
+		element.innerText = currentPlayer.token;
 	}
 	draw++;
-	console.log('draw =', draw);
 
 	checkWinner();
 	if (!gameEnded) {
 		element.style.color = 'white';
 		element.classList.remove('hover');
 		element.classList.add('no-hover');
+		console.log(element.innerText);
+		console.log(currentPlayer.token);
 		currentPlayer === playerOne ? (currentPlayer = playerTwo) : (currentPlayer = playerOne);
-		h2.innerHTML = currentPlayer.name;
+		h2.innerText = currentPlayer.name;
 	} else if (gameEnded) {
 		for (let i = 0; i < boxes.length; i++) {
 			boxes[i].classList.add('no-hover');
 		}
 		setTimeout(function() {
 			h2.style.display = 'none';
-			btn.innerHTML = 'AGAIN?';
+			btn.innerText = 'AGAIN';
 			btn.style.display = 'block';
 		}, 2000);
 	}
@@ -140,128 +143,87 @@ function setToken() {
 		}
 		setTimeout(function() {
 			h2.style.display = 'none';
-			btn.innerHTML = 'AGAIN?';
+			btn.innerText = 'AGAIN';
 			btn.style.display = 'block';
 		}, 2000);
 		gameEnded = false;
 	}
 }
 
+makeMouseEnterEvent();
+
+function winningLine(firstBox, secondBox, thirdBox) {
+	firstBox.classList.add('when-won');
+	firstBox.style.color = '#000001';
+	secondBox.classList.add('when-won');
+	secondBox.style.color = '#000001';
+	thirdBox.classList.add('when-won');
+	// voor mij onverklaarbare bug: als op 'black' zet, dan verdwijnt currentPlayer.token. Daarom #000001 van gemaakt en bij rest ook
+	thirdBox.style.color = '#000001';
+	h2.innerText = `${currentPlayer.name} wins!`;
+	gameEnded = true;
+}
 function checkWinner() {
 	//winst bovenste horizontale rij
 	if (
-		box1.innerHTML === currentPlayer.token &&
-		box2.innerHTML === currentPlayer.token &&
-		box3.innerHTML === currentPlayer.token
+		box1.innerText === currentPlayer.token &&
+		box2.innerText === currentPlayer.token &&
+		box3.innerText === currentPlayer.token
 	) {
-		box1.classList.add('when-won');
-		box1.style.color = '#000001';
-		box2.classList.add('when-won');
-		box2.style.color = '#000001';
-		box3.classList.add('when-won');
-		// voor mij onverklaarbare bug: als op 'black' zet, dan verdwijnt currentPlayer.token. Daarom #000001 van gemaakt en bij rest ook
-		box3.style.color = '#000001';
-		h2.innerHTML = `${currentPlayer.name} wins!`;
-		gameEnded = true;
+		winningLine(box1, box2, box3);
 
 		//winst middelste horizontale rij
 	} else if (
-		box4.innerHTML === currentPlayer.token &&
-		box5.innerHTML === currentPlayer.token &&
-		box6.innerHTML === currentPlayer.token
+		box4.innerText === currentPlayer.token &&
+		box5.innerText === currentPlayer.token &&
+		box6.innerText === currentPlayer.token
 	) {
-		box4.classList.add('when-won');
-		box4.style.color = '#000001';
-		box5.classList.add('when-won');
-		box5.style.color = '#000001';
-		box6.classList.add('when-won');
-		box6.style.color = '#000001';
-		h2.innerHTML = `${currentPlayer.name} wins!`;
-		gameEnded = true;
+		winningLine(box4, box5, box6);
 
 		//winst onderste horizontale rij
 	} else if (
-		box7.innerHTML === currentPlayer.token &&
-		box8.innerHTML === currentPlayer.token &&
-		box9.innerHTML === currentPlayer.token
+		box7.innerText === currentPlayer.token &&
+		box8.innerText === currentPlayer.token &&
+		box9.innerText === currentPlayer.token
 	) {
-		box7.classList.add('when-won');
-		box7.style.color = '#000001';
-		box8.classList.add('when-won');
-		box8.style.color = '#000001';
-		box9.classList.add('when-won');
-		box9.style.color = '#000001';
-		h2.innerHTML = `${currentPlayer.name} wins!`;
-		gameEnded = true;
+		winningLine(box7, box8, box9);
+
 		//winst linkse verticale rij
 	} else if (
-		box1.innerHTML === currentPlayer.token &&
-		box4.innerHTML === currentPlayer.token &&
-		box7.innerHTML === currentPlayer.token
+		box1.innerText === currentPlayer.token &&
+		box4.innerText === currentPlayer.token &&
+		box7.innerText === currentPlayer.token
 	) {
-		box1.classList.add('when-won');
-		box1.style.color = '#000001';
-		box4.classList.add('when-won');
-		box4.style.color = '#000001';
-		box7.classList.add('when-won');
-		box7.style.color = '#000001';
-		h2.innerHTML = `${currentPlayer.name} wins!`;
-		gameEnded = true;
+		winningLine(box1, box4, box7);
+
 		//winst middelste verticale rij
 	} else if (
-		box2.innerHTML === currentPlayer.token &&
-		box5.innerHTML === currentPlayer.token &&
-		box8.innerHTML === currentPlayer.token
+		box2.innerText === currentPlayer.token &&
+		box5.innerText === currentPlayer.token &&
+		box8.innerText === currentPlayer.token
 	) {
-		box2.classList.add('when-won');
-		box2.style.color = '#000001';
-		box5.classList.add('when-won');
-		box5.style.color = '#000001';
-		box8.classList.add('when-won');
-		box8.style.color = '#000001';
-		h2.innerHTML = `${currentPlayer.name} wins!`;
-		gameEnded = true;
+		winningLine(box2, box5, box8);
+
 		//winst rechtse verticale rij
 	} else if (
-		box3.innerHTML === currentPlayer.token &&
-		box6.innerHTML === currentPlayer.token &&
-		box9.innerHTML === currentPlayer.token
+		box3.innerText === currentPlayer.token &&
+		box6.innerText === currentPlayer.token &&
+		box9.innerText === currentPlayer.token
 	) {
-		box3.classList.add('when-won');
-		box3.style.color = '#000001';
-		box6.classList.add('when-won');
-		box6.style.color = '#000001';
-		box9.classList.add('when-won');
-		box9.style.color = '#000001';
-		h2.innerHTML = `${currentPlayer.name} wins!`;
-		gameEnded = true;
+		winningLine(box3, box6, box9);
 		//winst diagonale rij vanaf links
 	} else if (
-		box1.innerHTML === currentPlayer.token &&
-		box5.innerHTML === currentPlayer.token &&
-		box9.innerHTML === currentPlayer.token
+		box1.innerText === currentPlayer.token &&
+		box5.innerText === currentPlayer.token &&
+		box9.innerText === currentPlayer.token
 	) {
-		box1.classList.add('when-won');
-		box1.style.color = '#000001';
-		box5.classList.add('when-won');
-		box5.style.color = '#000001';
-		box9.classList.add('when-won');
-		box9.style.color = '#000001';
-		h2.innerHTML = `${currentPlayer.name} wins!`;
-		gameEnded = true;
+		winningLine(box1, box5, box9);
 		//winst diagonale rij vanaf rechts
 	} else if (
-		box3.innerHTML === currentPlayer.token &&
-		box5.innerHTML === currentPlayer.token &&
-		box7.innerHTML === currentPlayer.token
+		box3.innerText === currentPlayer.token &&
+		box5.innerText === currentPlayer.token &&
+		box7.innerText === currentPlayer.token
 	) {
-		box3.classList.add('when-won');
-		box3.style.color = '#000001';
-		box5.classList.add('when-won');
-		box5.style.color = '#000001';
-		box7.classList.add('when-won');
-		box7.style.color = '#000001';
-		h2.innerHTML = `${currentPlayer.name} wins!`;
-		gameEnded = true;
+		winningLine(box3, box5, box7);
 	}
 }
